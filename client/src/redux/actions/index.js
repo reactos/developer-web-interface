51fc7fe026fd3a35549ede853d388c4cdbe86256
Ayush Kumar Sinha <ayush_snha@rediffmail.com>
2019-06-29 11:30:05 +0530

    export modification in action/index.js

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,72 +1,57 @@
 import { COMMITS, BRANCHES, PULLS } from '../constants';
 
-const loadCommits = newPage => ({
+export const loadCommits = newPage => ({
  type: COMMITS.LOAD,
  newPage
 });
 
-const setCommits = commits => ({
+export const setCommits = commits => ({
  type: COMMITS.LOAD_SUCCESS,
  commits
 });
 
-const setCommitsError = error => ({
+export const setCommitsError = error => ({
  type: COMMITS.LOAD_FAIL,
  error
 });
 
-const setPages = (next, prev) => ({
+export const setPages = (next, prev) => ({
  type: 'PAGE_LOAD_SUCCESS',
  next,
  prev
 });
-const loadBranches = () => ({
+export const loadBranches = () => ({
  type: BRANCHES.LOAD
 });
 
-const setBranches = branches => ({
+export const setBranches = branches => ({
  type: BRANCHES.LOAD_SUCCESS,
  branches
 });
-const currBranch = branch => ({
+export const currBranch = branch => ({
  type: BRANCHES.CURRENT,
  branch
 });
 
-const setBranchesError = error => ({
+export const setBranchesError = error => ({
  type: BRANCHES.LOAD_FAIL,
  error
 });
-const currState = pullState => ({
+export const currState = pullState => ({
  type: PULLS.CURRENT,
  pullState
 });
-const loadPulls = newPage => ({
+export const loadPulls = newPage => ({
  type: PULLS.LOAD,
  newPage
 });
 
-const setPulls = pulls => ({
+export const setPulls = pulls => ({
  type: PULLS.LOAD_SUCCESS,
  pulls
 });
 
-const setPullsError = error => ({
+export const setPullsError = error => ({
  type: PULLS.LOAD_FAIL,
  error
 });
-
-export {
- loadCommits,
- setCommits,
- setCommitsError,
- setPages,
- loadBranches,
- setBranches,
- setBranchesError,
- currBranch,
- loadPulls,
- currState,
- setPulls,
- setPullsError
-};

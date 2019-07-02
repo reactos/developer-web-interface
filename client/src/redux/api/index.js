701c263948941e10027900796e732f630526a2f6
Ayush Kumar Sinha <ayush_snha@rediffmail.com>
2019-07-02 13:52:10 +0530

    client call to fetch build data

--- client/src/redux/api/index.js
+++ client/src/redux/api/index.js
@@ -1,4 +1,4 @@
-const fetchCommits = async (sha, page) => {
+export const fetchCommits = async (sha, page) => {
  const response = await fetch(`/api/commits?sha=${sha}&page=${page}`);
  const data = await response.json();
  if (response.status >= 400) {
@@ -7,7 +7,34 @@ const fetchCommits = async (sha, page) => {
  return data;
 };
 
-const fetchBranches = async () => {
+export const fetchBuildSets = async () => {
+ const response = await fetch('/api/buildsets');
+ const data = await response.json();
+ if (response.status >= 400) {
+  throw new Error(data.errors);
+ }
+ return data.buildsets;
+};
+
+export const fetchBuildReq = async () => {
+ const response = await fetch('/api/buildreq');
+ const data = await response.json();
+ if (response.status >= 400) {
+  throw new Error(data.errors);
+ }
+ return data.buildrequests;
+};
+
+export const fetchBuilds = async () => {
+ const response = await fetch('/api/builds');
+ const data = await response.json();
+ if (response.status >= 400) {
+  throw new Error(data.errors);
+ }
+ return data.builds;
+};
+
+export const fetchBranches = async () => {
  const response = await fetch('/api/branches');
  const data = await response.json();
  if (response.status >= 400) {
@@ -15,7 +42,7 @@ const fetchBranches = async () => {
  }
  return data;
 };
-const fetchPulls = async (state, page) => {
+export const fetchPulls = async (state, page) => {
  const response = await fetch(`/api/pulls?state=${state}&page=${page}`);
  const data = await response.json();
  if (response.status >= 400) {
@@ -23,5 +50,3 @@ const fetchPulls = async (state, page) => {
  }
  return data;
 };
-
-export { fetchCommits, fetchBranches, fetchPulls };

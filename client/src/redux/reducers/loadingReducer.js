bc58929a90af7ffaff2c25ef8767f6b471bddd9f
Ayush Kumar Sinha <ayush_snha@rediffmail.com>
2019-07-02 13:58:59 +0530

    modifying loading reducer

--- client/src/redux/reducers/loadingReducer.js
+++ client/src/redux/reducers/loadingReducer.js
@@ -1,19 +1,34 @@
-import { COMMITS, BRANCHES, PULLS } from '../constants';
+import { COMMITS, BRANCHES, PULLS, BUILD_DATA } from '../constants';
 
 const loadingReducer = (state = { newPage: 1, load: false }, action) => {
  switch (action.type) {
   case COMMITS.LOAD:
    return { newPage: action.newPage, load: true };
+
   case PULLS.LOAD:
    return { newPage: action.newPage, load: true };
-  case BRANCHES.LOAD_SUCCESS:
+
+  case BRANCHES.LOAD:
    return { newPage: action.newPage, load: true };
+
+  case BUILD_DATA.LOAD:
+   return { newPage: action.newPage, load: true };
+
   case COMMITS.LOAD_SUCCESS:
    return { ...state, load: false };
+
   case PULLS.LOAD_SUCCESS:
    return { ...state, load: false };
+
+  case BRANCHES.LOAD_SUCCESS:
+   return { newPage: action.newPage, load: true };
+
+  case BUILD_DATA.LOAD_SUCCESS:
+   return { load: true };
+
   case COMMITS.LOAD_FAIL:
    return { ...state, load: false };
+
   case PULLS.LOAD_FAIL:
    return { ...state, load: false };
 

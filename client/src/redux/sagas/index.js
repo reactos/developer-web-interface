7bc200c660577e65a85f796bc13000c29c544cf3
Ayush Kumar Sinha <ayush_snha@rediffmail.com>
2019-07-02 13:54:55 +0530

    calling build saga from root saga

--- client/src/redux/sagas/index.js
+++ client/src/redux/sagas/index.js
@@ -3,7 +3,8 @@ import { all } from 'redux-saga/effects';
 import commitsSaga from './commitsSaga';
 import branchesSaga from './branchesSaga';
 import pullsSaga from './pullsSaga';
+import buildsSaga from './buildsSaga';
 
 export default function* rootSaga() {
- yield all([commitsSaga(), branchesSaga(), pullsSaga()]);
+ yield all([commitsSaga(), branchesSaga(), pullsSaga(), buildsSaga()]);
 }

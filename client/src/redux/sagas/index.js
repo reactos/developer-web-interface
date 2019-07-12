96277e8bc4c5bef55629277226f2695acd741b40
Ayush Kumar Sinha <ayush_snha@rediffmail.com>
2019-07-12 10:40:10 +0530

    modifying root saga to include buildreq saga

--- client/src/redux/sagas/index.js
+++ client/src/redux/sagas/index.js
@@ -4,7 +4,14 @@ import commitsSaga from './commitsSaga';
 import branchesSaga from './branchesSaga';
 import pullsSaga from './pullsSaga';
 import buildsSaga from './buildsSaga';
+import buildReqSaga from './buildReqSaga';
 
 export default function* rootSaga() {
- yield all([commitsSaga(), branchesSaga(), pullsSaga(), buildsSaga()]);
+ yield all([
+  commitsSaga(),
+  branchesSaga(),
+  pullsSaga(),
+  buildsSaga(),
+  buildReqSaga()
+ ]);
 }

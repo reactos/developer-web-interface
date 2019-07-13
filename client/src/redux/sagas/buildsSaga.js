8f174e09ea7a67e9b80fd7160e40ae016bc14655
Ayush Kumar Sinha <ayush_snha@rediffmail.com>
2019-07-13 11:16:30 +0530

    builds saga

--- client/src/redux/sagas/buildsSaga.js
+++ client/src/redux/sagas/buildsSaga.js
@@ -1,16 +1,28 @@
-import { takeEvery, call, put } from 'redux-saga/effects';
-import { BUILD_DATA } from '../constants';
-import { fetchBuildSets } from '../api';
-import { setBuildSets, setBuildSetsError } from '../actions';
+import { takeEvery, call, select, put } from 'redux-saga/effects';
+import { fetchBuilds } from '../api';
+import { setBuildSetsError, setBuilds } from '../actions';
+const bsid = state => state.bsid;
+
+function buildStr(build) {
+ let str = build
+  .map(build => 'buildrequestid__contains=' + build.buildrequestid)
+  .join('&');
+ return str;
+}
+
 function* handleBuildsLoad() {
  try {
-  const buildSets = yield call(fetchBuildSets);
-  yield put(setBuildSets(buildSets));
+  const build = yield select(bsid);
+  var str = buildStr(build);
+  if (str) {
+   let build = yield call(fetchBuilds, str);
+   yield put(setBuilds(build));
+  }
  } catch (error) {
   yield put(setBuildSetsError(error.toString()));
  }
 }
 
 export default function* watchBuildsLoad() {
- yield takeEvery(BUILD_DATA.LOAD, handleBuildsLoad);
+ yield takeEvery('BUILD_SET_ID_SUCCESS', handleBuildsLoad);
 }

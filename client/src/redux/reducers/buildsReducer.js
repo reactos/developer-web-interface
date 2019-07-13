e9022b37978d1368be4c65144d55982a313dd90b
Ayush Kumar Sinha <ayush_snha@rediffmail.com>
2019-07-13 11:07:05 +0530

    cleaning

--- client/src/redux/reducers/buildsReducer.js
+++ client/src/redux/reducers/buildsReducer.js
@@ -1,5 +1,5 @@
+//handling the buildset endpoint
 import { BUILD_DATA } from '../constants';
-
 const buildsReducer = (state = [], action) => {
  if (action.type === BUILD_DATA.LOAD_SUCCESS) {
   return [...action.buildSets];

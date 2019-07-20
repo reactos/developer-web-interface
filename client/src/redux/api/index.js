2b677d9d579a20aa692c73de335b6cb85c2f2257
Ayush Kumar Sinha <ayush_snha@rediffmail.com>
2019-07-20 16:14:09 +0530

    api modifs

--- client/src/redux/api/index.js
+++ client/src/redux/api/index.js
@@ -34,6 +34,15 @@ export const fetchBuilds = async str => {
  return data.builds;
 };
 
+export const fetchBuilders = async () => {
+ const response = await fetch('/api/builders');
+ const data = await response.json();
+ if (response.status >= 400) {
+  throw new Error(data.errors);
+ }
+ return data;
+};
+
 export const fetchBranches = async () => {
  const response = await fetch('/api/branches');
  const data = await response.json();

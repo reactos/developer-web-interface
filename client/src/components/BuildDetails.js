5cbf49cbacbe9488104125b50ed7c3f9f28ae1a6
Ayush Kumar Sinha <ayush_snha@rediffmail.com>
2019-07-23 22:01:39 +0530

    changing into functional component and mapping stateToProps

--- client/src/components/BuildDetails.js
+++ client/src/components/BuildDetails.js
@@ -1,47 +1,50 @@
 import React from 'react';
-class BuildDetails extends React.Component {
-  renderBuild = build => {
+import { connect } from 'react-redux';
+function BuildDetails(props) {
+  const renderBuilds = builds => {
     return (
-      <React.Fragment key={build.buildid}>
+      <React.Fragment key={builds.buildid}>
         <div className='col-sm-3'>
           <a
             target='_blank'
             rel='noreferrer noopener'
             href={`https://build.reactos.org/#builders/${
-              build.builderid
-            }/builds/${build.number}`}
+              builds.builderid
+            }/builds/${builds.number}`}
           >
-            {this.props.builders[build.builderid - 1].name}
+            {props.builders[builds.builderid - 1].name}
           </a>
         </div>
         <div className='col-sm-3'>
-          {build.state_string}
-          {build.state_string === 'build successful' ? (
+          {builds.state_string}
+          {builds.state_string === 'build successful' ? (
             <i className='fa fa-check' />
           ) : (
             <i />
           )}
         </div>
-        <div className='col-sm-3'>started_at:{build.started_at}</div>
-        <div className='col-sm-3'>complete_at:{build.complete_at}</div>
+        <div className='col-sm-3'>started_at:{builds.started_at}</div>
+        <div className='col-sm-3'>complete_at:{builds.complete_at}</div>
       </React.Fragment>
     );
   };
-  render() {
-    return this.props.build ? (
-      <React.Fragment>
-        {this.props.build.length > 0 ? (
-          <div className='row'>{this.props.build.map(this.renderBuild)}</div>
-        ) : (
-          <p>
-            <strong>No data Exists</strong>
-          </p>
-        )}
-      </React.Fragment>
-    ) : (
-      <h6>No data Exists</h6>
-    );
-  }
+
+  return props.builds ? (
+    <React.Fragment>
+      {props.builds.length > 0 ? (
+        <div className='row'>{props.builds.map(renderBuilds)}</div>
+      ) : (
+        <p>
+          <strong>No data Exists</strong>
+        </p>
+      )}
+    </React.Fragment>
+  ) : (
+    <h6>No data Exists</h6>
+  );
 }
 
-export default BuildDetails;
+const mapStateToProps = ({ builders }) => ({
+  builders
+});
+export default connect(mapStateToProps)(BuildDetails);

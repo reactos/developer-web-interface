7bd218c3d6ac1039b9c6fbc85ffe5ed02ebe73bd
Victor Perevertkin <victor@perevertkin.ru>
2019-08-15 19:35:29 +0300

    Added redux-logger

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,20 +1,22 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import rootReducer from '../reducers';
 import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
+import rootReducer from '../reducers';
 import rootSaga from '../sagas';
+
 const configureStore = () => {
- const sagaMiddleware = createSagaMiddleware();
- const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__
-   ? compose(
-      applyMiddleware(sagaMiddleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__()
-     )
-   : applyMiddleware(sagaMiddleware)
- );
- sagaMiddleware.run(rootSaga);
- return store;
+  const sagaMiddleware = createSagaMiddleware();
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+  const middlewares = [sagaMiddleware];
+
+  if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger)
+  }
+
+  const store = createStore(rootReducer, undefined, composeEnhancers(applyMiddleware(...middlewares)));
+
+  sagaMiddleware.run(rootSaga);
+  return store;
 };
 
 export default configureStore;

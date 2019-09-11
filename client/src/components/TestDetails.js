f53f79e23b5d2808091da4a9ed9dedfd71aa5462
shooter0106 <shooter0106@gmail.com>
2019-09-11 22:27:49 +0700

    Display test count changes from previous commit

--- client/src/components/TestDetails.js
+++ client/src/components/TestDetails.js
@@ -1,6 +1,28 @@
 import React from 'react';
 
-function renderTest(test) {
+function renderCountChange(test, previousTestCount) {
+  if (!previousTestCount) {
+    return;
+  }
+
+  let countDifference = test.count - previousTestCount;
+
+  if (countDifference > 0) {
+    return (
+      <sup className='text-success'>
+        +{countDifference}
+      </sup>
+    );
+  } else if (countDifference < 0) {
+    return (
+      <sup className='text-danger'>
+        {countDifference}
+      </sup>
+    );
+  }
+}
+
+function renderTest(test, previousTests) {
   return (
     <React.Fragment key={test.id}>
       <div className='col-sm-2'>Test id: {test.id}</div>
@@ -13,7 +35,7 @@ function renderTest(test) {
           {test.source}
         </a>
       </div>
-      <div className='col-sm-3'>Count: {test.count}</div>
+      <div className='col-sm-3'>Count: {test.count}{renderCountChange(test, previousTests[test.source])}</div>
       <div className='col-sm-3'>Failures: {test.failures}</div>
     </React.Fragment>
   );
@@ -23,7 +45,7 @@ function TestDetails(props) {
   return (
     <React.Fragment>
       {props.tests.length > 0 ? (
-        <div className='row'>{props.tests.map(renderTest)}</div>
+        <div className='row'>{props.tests.map(test => renderTest(test, props.previousTests))}</div>
       ) : (
         <p>
           <strong>No data Exists</strong>

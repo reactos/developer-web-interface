cf08a1e0c03364d9067d049dbae641528955adc5
Victor Perevertkin <victor.perevertkin@reactos.org>
2020-02-14 01:00:10 +0300

    Use react-router in application, for keeping url updated while navigating Remove tabs and move Commits and Pull Requests links to navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import Header from './components/Header';
-import TabEX from './components/TabEX';
+import Commits from './components/Commits';
+import Pulls from './components/Pulls';
 import configureStore from './redux/store';
 const store = configureStore();
 
-function App() {
- return (
-  <Provider store={store}>
-   <div>
-    <Header />
-    <TabEX />
-   </div>
-  </Provider>
- );
-}
-
-export default App;
+export default function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <div>
+          <Header/>
+          <Switch>
+            <Route path="/pulls"><Pulls/></Route>
+            <Route path="/"><Commits/></Route>
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
+  );
+};
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import './styles/Header.css';
+import { Link, useRouteMatch } from 'react-router-dom';
+import cn from 'classnames';
 import { Collapse, NavbarToggler } from 'reactstrap';
 
-class Header extends React.Component {
+function NavLink({ label, to, activeOnlyWhenExact }) {
+  let match = useRouteMatch({
+    path: to,
+    exact: activeOnlyWhenExact
+  });
+
+  return (
+    <li className={cn("nav-item", {active: match})}>
+      <Link className="nav-link" to={to}>{label}</Link>
+    </li>
+  );
+}
+
+class Header extends React.PureComponent {
   constructor(props) {
     super(props);
 
@@ -11,49 +25,45 @@ class Header extends React.Component {
       isOpen: false
     };
   }
+
   toggle() {
     this.setState({
       isOpen: !this.state.isOpen
     });
   }
+
   render() {
     return (
-      <div>
-        <nav className='navbar navbar-expand-sm bg-dark navbar-dark'>
-          <a className='navbar-brand' href='https://www.reactos.org'>
-            <img
-              src='https://reactos.org/wiki/images/thumb/0/02/ReactOS_logo.png/300px-ReactOS_logo.png'
-              alt='logo'
-            />
-          </a>
-          <span className='name'>
-            <h3>Developer Interface</h3>
-          </span>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <ul className='navbar-nav ml-auto'>
-              <li className='nav-item'>
-                <a className='nav-link' href='https://github.com/reactos'>
-                  <i className='fa fa-github' />
-                  Github
-                </a>
-              </li>
-              <li className='nav-item'>
-                <a className='nav-link' href='https://reactos.org/testman'>
-                  <i className='fa fa-cog' />
-                  Testman
-                </a>
-              </li>
-              <li className='nav-item'>
-                <a className='nav-link' href='https://build.reactos.org/'>
-                  <i className='fa fa-wrench' />
-                  Buildbot
-                </a>
-              </li>
-            </ul>
-          </Collapse>
-        </nav>
-      </div>
+      <nav className='navbar navbar-expand-sm bg-dark navbar-dark'>
+        <span className="navbar-brand">Developer Interface</span>
+
+        <ul className="navbar-nav ml-auto">
+          <NavLink to="/" label="Commits" activeOnlyWhenExact/>
+          <NavLink to="/pulls" label="Pull Requests"/>
+        </ul>
+
+        <NavbarToggler onClick={this.toggle} />
+
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <ul className='navbar-nav ml-auto'>
+            <li className='nav-item'>
+              <a className='nav-link' href='https://github.com/reactos/reactos'>
+                <i className='fa fa-github' />{" "}Github
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='https://reactos.org/testman'>
+                <i className='fa fa-cog' />{" "}Testman
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='https://build.reactos.org/'>
+                <i className='fa fa-wrench' />{" "}Buildbot
+              </a>
+            </li>
+          </ul>
+        </Collapse>
+      </nav>
     );
   }
 }
